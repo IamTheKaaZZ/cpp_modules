657741ee803caa04cefa175ebdf8ff4e3a0d8773
Ben Costers <bcosters@c1r15s7.42lisboa.com>
2021-11-19 12:04:40 +0000

    Started 02/ex00

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -1,15 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcosters <bcosters@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 11:46:45 by bcosters          #+#    #+#             */
+/*   Updated: 2021/11/19 12:04:19 by bcosters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 /*
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
 
-Fixed::Fixed()
+Fixed::Fixed() : _fpVal(0)
 {
+	std::cout << "Default constructor called." << std::endl;
 }
 
-Fixed::Fixed( const Fixed & src )
+Fixed::Fixed( const Fixed & src ) : _fpVal(src._fpVal)
 {
+	std::cout << "Copy constructor called." << std::endl;
 }
 
 
@@ -19,6 +33,7 @@ Fixed::Fixed( const Fixed & src )
 
 Fixed::~Fixed()
 {
+	std::cout << "Destructor called." << std::endl;
 }
 
 
@@ -28,15 +43,17 @@ Fixed::~Fixed()
 
 Fixed &				Fixed::operator=( Fixed const & rhs )
 {
-	//if ( this != &rhs )
-	//{
-		//this->_value = rhs.getValue();
-	//}
+	std::cout << "Assignment operater called." << std::endl;
+	if ( this != &rhs )
+	{
+		this->_fpVal = rhs.getRawBits();
+	}
 	return *this;
 }
 
 std::ostream &			operator<<( std::ostream & o, Fixed const & i )
 {
+	std::cout << "Output overload called." << std::endl;
 	//o << "Value = " << i.getValue();
 	return o;
 }
@@ -46,6 +63,13 @@ std::ostream &			operator<<( std::ostream & o, Fixed const & i )
 ** --------------------------------- METHODS ----------------------------------
 */
 
+int		Fixed::getRawBits() const {
+	return this->_fpVal;
+}
+
+void	Fixed::setRawBits(int const raw) {
+	this->_fpVal = raw;
+}
 
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcosters <bcosters@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 11:47:04 by bcosters          #+#    #+#             */
+/*   Updated: 2021/11/19 12:01:08 by bcosters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
@@ -15,7 +27,13 @@ class Fixed
 
 		Fixed &		operator=( Fixed const & rhs );
 
+		int			getRawBits() const;
+		void		setRawBits(int const raw);
+
 	private:
+
+		int					_fpVal;
+		static const int 	_numFracBits = 8;
 
 };
 
