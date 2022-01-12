d1fcf684f667470e6d898b9098c12840603d9e04
bcosters <ben.costers@hotmail.com>
2022-01-12 11:35:51 +0000

    Getting there

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,29 +6,39 @@
 /*   By: bcosters <bcosters@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 16:48:26 by bcosters          #+#    #+#             */
-/*   Updated: 2022/01/11 18:31:15 by bcosters         ###   ########.fr       */
+/*   Updated: 2022/01/12 11:34:39 by bcosters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <iostream>
 # include <algorithm>
 # include <exception>
+# include <string>
+# include <sstream>
 
 class NotFoundException : public std::exception {
     public:
-        virtual	const char*	what() const throw() {
-            return ("The int cannot be found.");
+        NotFoundException(int notFound) {
+            _ss << "The number " << notFound << " cannot be found";
+            _ss >> _msg;
         }
+        virtual	const char*	what() const throw() {
+            return _msg.c_str();
+        }
+    private:
+        std::stringstream   _ss;
+        std::string         _msg;
 };
 
 template<typename T>
-typename T::iterator  easyfind(T& c, int toFind) {
-    typename T::iterator  fIt = std::find_first_of(c.begin(), c.end(), toFind, toFind);
+typename T::const_iterator    easyfind(const T& c, const int toFind) {
+    typename T::const_iterator  fIt = std::find(c.begin(), c.end(), toFind);
     if (fIt != c.end())
         return fIt;
-    else throw NotFoundException();
+    else throw NotFoundException(toFind);
 }
 
 #endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bcosters <bcosters@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 17:20:37 by bcosters          #+#    #+#             */
-/*   Updated: 2022/01/11 18:18:22 by bcosters         ###   ########.fr       */
+/*   Updated: 2022/01/12 11:34:47 by bcosters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ using std::vector;
 using std::list;
 using std::for_each;
 
-template<class T>
-int getIndex(T& cont, typename T::iterator it) {
-    typename T::iterator    bIt = cont.begin();
+template<typename T>
+int getIndex(T& cont, typename T::const_iterator it) {
+    typename T::const_iterator    bIt = cont.begin();
     int index = std::distance(bIt, it);
     return index;
 }
@@ -38,17 +38,28 @@ int main() {
     int ints[] = {1, 2, 2, 3, 3, 3, 5, 5, 5, 6};
     vector<int>    v(ints, ints + sizeof(ints) / sizeof(int));
     list<int>      l(ints, ints + sizeof(ints) / sizeof(int));
-    vector<int>::iterator  vIt;
-    list<int>::iterator    lIt;
+    vector<int>::const_iterator  vIt;
+    list<int>::const_iterator    lIt;
 
     cout << "The vector contains:";
     for_each(v.begin(), v.end(), printInt);
     cout << endl;
     vIt = easyfind(v, 3);
-    cout << "Found the first occurance of 3 at index = " << getIndex(v, vIt) << endl;
+    if (vIt != v.end())
+        cout << "Found 3 in the vector ad index: " << getIndex(v, vIt) << endl;
     cout << "The list contains:";
     for_each(l.begin(), l.end(), printInt);
     cout << endl;
     lIt = easyfind(l, 3);
-    cout << "Found the first occurance of 3 at index = " << getIndex(l, lIt) << endl;
+    if (vIt != v.end())
+        cout << "Found 3 in the list at index: " << getIndex(l, lIt) << endl;
+    try {
+        cout << "The vector contains:";
+        for_each(v.begin(), v.end(), printInt);
+        cout << endl;
+        vIt = easyfind(v, 42);
+    }
+    catch (NotFoundException & e) {
+        cout << e.what() << endl;
+    }
 }
